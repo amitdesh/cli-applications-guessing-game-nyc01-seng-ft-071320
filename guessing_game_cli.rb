dbd08308c222c5318b02a78b80412c385cf9d0d7
Amit Deshpande <amitdesh2014@gmail.com>
2020-07-12 15:10:33 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,1 +1,38 @@
-# Code your solution here!
+def generate_random_number
+  rand_num = rand(1..6)
+  return rand_num
+end
+
+def guess_prompt
+  puts "Please take a guess at the random number."
+end
+
+def guess_attempt
+  user_guess= gets.chomp
+  return user_guess
+end
+
+def correct_guess
+  puts "You guessed the correct number!"
+end
+
+def wrong_guess(rand_num)
+  puts "Sorry! The computer guessed #{rand_num}".
+end
+
+def exit_game
+  puts "Goodbye!"
+end
+
+def run_guessing_gamw
+  rand_num = generate_random_number
+  guess_prompt
+  input = guess_attempt
+  if input == rand_num
+    correct_guess 
+  elsif input != rand_num
+    wrong_guess(rand_num)
+  elsif input == "exit"
+    exit_game
+  end
+end
